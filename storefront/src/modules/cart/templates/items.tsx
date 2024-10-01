06da4f9021d00df7431e379cb7aa02601a918f80
Victor Gerbrands <victorgerbrands@gmail.com>
2024-10-01 12:05:10 +0200

    feat: cart item value input

--- storefront/src/modules/cart/templates/items.tsx
+++ storefront/src/modules/cart/templates/items.tsx
@@ -39,34 +39,6 @@ const ItemsTemplate = ({ cart }: ItemsTemplateProps) => {
           </Text>
         </div>
       </Container>
-      {/* <Table>
-        <Table.Header className="border-t-0">
-          <Table.Row className="text-ui-fg-subtle txt-medium-plus">
-            <Table.HeaderCell className="!pl-0">Item</Table.HeaderCell>
-            <Table.HeaderCell></Table.HeaderCell>
-            <Table.HeaderCell>Quantity</Table.HeaderCell>
-            <Table.HeaderCell className="hidden small:table-cell">
-              Price
-            </Table.HeaderCell>
-            <Table.HeaderCell className="!pr-0 text-right">
-              Total
-            </Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {items
-            ? items
-                .sort((a, b) => {
-                  return (a.created_at ?? "") > (b.created_at ?? "") ? -1 : 1
-                })
-                .map((item) => {
-                  return <Item key={item.id} item={item} />
-                })
-            : repeat(5).map((i) => {
-                return <SkeletonLineItem key={i} />
-              })}
-        </Table.Body>
-      </Table> */}
     </div>
   )
 }
