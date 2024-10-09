590e85eaa61d41b1ab91f727daffc528c8b1ff70
Riqwan Thamir <rmthamir@gmail.com>
2024-10-09 10:15:02 +0200

    chore: fix tailwind discovery

--- apps/storefront/tailwind.config.js
+++ apps/storefront/tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
     "./src/modules/**/*.{js,ts,jsx,tsx}",
     "./node_modules/@medusajs/ui/dist/**/*.{js,jsx,ts,tsx}",
+    "../../node_modules/@medusajs/ui/dist/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
