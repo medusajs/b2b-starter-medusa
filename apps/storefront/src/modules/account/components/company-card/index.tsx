eb06f69c135bff35a9a47de3314525b9ff10747d
Victor Gerbrands <victorgerbrands@gmail.com>
2024-10-11 12:46:20 +0200

    feat: edit transitions

--- apps/storefront/src/modules/account/components/company-card/index.tsx
+++ apps/storefront/src/modules/account/components/company-card/index.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Container, Text, Toaster, toast } from "@medusajs/ui"
+import { Container, Text, Toaster, clx, toast } from "@medusajs/ui"
 import Input from "@modules/common/components/input"
 import Button from "@modules/common/components/button"
 import Select from "@modules/common/components/native-select"
@@ -49,199 +49,209 @@ const CompanyCard = ({
   )
 
   return (
-    <div className="h-fit transition-all duration-300 ease-in-out">
-      <Container className="p-0 overflow-hidden ">
-        {isEditing ? (
-          <form
-            className="grid grid-cols-2 gap-4 p-4 border-b border-neutral-200"
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                e.preventDefault()
-                handleSave()
+    <div className="h-fit">
+      <Container className="p-0 overflow-hidden">
+        <form
+          className={clx(
+            "grid grid-cols-2 gap-4 border-b border-neutral-200 overflow-hidden transition-all duration-300 ease-in-out ",
+            {
+              "max-h-[422px] opacity-100 p-4": isEditing,
+              "max-h-0 opacity-0": !isEditing,
+            }
+          )}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              e.preventDefault()
+              handleSave()
+            }
+          }}
+        >
+          <div className="flex flex-col gap-y-2">
+            <Text className="font-medium text-neutral-950">Company Name</Text>
+            <Input
+              label="Company Name"
+              name="name"
+              value={companyData.name}
+              onChange={(e) =>
+                setCompanyData({ ...companyData, name: e.target.value })
               }
-            }}
-          >
-            <div className="flex flex-col gap-y-2">
-              <Text className="font-medium text-neutral-950">Company Name</Text>
-              <Input
-                label="Company Name"
-                name="name"
-                value={companyData.name}
-                onChange={(e) =>
-                  setCompanyData({ ...companyData, name: e.target.value })
-                }
-              />
-            </div>
-            <div className="flex flex-col gap-y-2">
-              <Text className="font-medium text-neutral-950">Email</Text>
-              <Input
-                label="Email"
-                name="email"
-                value={companyData.email}
-                onChange={(e) =>
-                  setCompanyData({ ...companyData, email: e.target.value })
-                }
-              />
-            </div>
-            <div className="flex flex-col gap-y-2">
-              <Text className="font-medium text-neutral-950">Phone</Text>
-              <Input
-                label="Phone"
-                name="phone"
-                value={companyData.phone}
-                onChange={(e) =>
-                  setCompanyData({ ...companyData, phone: e.target.value })
-                }
-              />
-            </div>
-            <div className="flex flex-col gap-y-2">
-              <Text className="font-medium text-neutral-950">Address</Text>
-              <Input
-                label="Address"
-                name="address"
-                value={companyData.address || ""}
-                onChange={(e) =>
-                  setCompanyData({ ...companyData, address: e.target.value })
-                }
-              />
-            </div>
-            <div className="flex flex-col gap-y-2">
-              <Text className="font-medium text-neutral-950">City</Text>
-              <Input
-                label="City"
-                name="city"
-                value={companyData.city || ""}
-                onChange={(e) =>
-                  setCompanyData({ ...companyData, city: e.target.value })
-                }
-              />
-            </div>
-            <div className="flex flex-col gap-y-2">
-              <Text className="font-medium text-neutral-950">State</Text>
-              <Input
-                label="State"
-                name="state"
-                value={companyData.state || ""}
-                onChange={(e) =>
-                  setCompanyData({ ...companyData, state: e.target.value })
-                }
-              />
-            </div>
-            <div className="flex flex-col gap-y-2">
-              <Text className="font-medium text-neutral-950">Zip</Text>
-              <Input
-                label="Zip"
-                name="zip"
-                value={companyData.zip || ""}
-                onChange={(e) =>
-                  setCompanyData({ ...companyData, zip: e.target.value })
-                }
-              />
-            </div>
-            <div className="flex flex-col gap-y-2">
-              <Text className="font-medium text-neutral-950">Country</Text>
-              <Select
-                name="country"
-                value={companyData.country || ""}
-                onChange={(e) =>
-                  setCompanyData({ ...companyData, country: e.target.value })
-                }
-              >
-                {countriesInRegions.map(
-                  (country) =>
-                    country && (
-                      <option key={country.id} value={country.id}>
-                        {country.name}
-                      </option>
-                    )
-                )}
-              </Select>
-            </div>
-            <div className="flex flex-col gap-y-2">
-              <Text className="font-medium text-neutral-950">Currency</Text>
-              <Select
-                name="currency_code"
-                value={companyData.currency_code || ""}
-                onChange={(e) =>
-                  setCompanyData({
-                    ...companyData,
-                    currency_code: e.target.value as string,
-                  })
-                }
-              >
-                {currenciesInRegions.map((currency) => (
-                  <option key={currency} value={currency}>
-                    {currency.toUpperCase()} ({currencySymbolMap[currency]})
-                  </option>
-                ))}
-              </Select>
-            </div>
-            <div className="flex flex-col gap-y-2">
-              <Text className="font-medium text-neutral-950">
-                Spending Limit Reset Frequency
-              </Text>
-              <Select
-                name="spending_limit_reset_frequency"
-                value={companyData.spending_limit_reset_frequency}
-                onChange={(e) =>
-                  setCompanyData({
-                    ...companyData,
-                    spending_limit_reset_frequency: e.target
-                      .value as ModuleCompanySpendingLimitResetFrequency,
-                  })
-                }
-              >
-                {Object.values(ModuleCompanySpendingLimitResetFrequency).map(
-                  (value) => (
-                    <option key={value} value={value}>
-                      {value.charAt(0).toUpperCase() + value.slice(1)}
+            />
+          </div>
+          <div className="flex flex-col gap-y-2">
+            <Text className="font-medium text-neutral-950">Email</Text>
+            <Input
+              label="Email"
+              name="email"
+              value={companyData.email}
+              onChange={(e) =>
+                setCompanyData({ ...companyData, email: e.target.value })
+              }
+            />
+          </div>
+          <div className="flex flex-col gap-y-2">
+            <Text className="font-medium text-neutral-950">Phone</Text>
+            <Input
+              label="Phone"
+              name="phone"
+              value={companyData.phone}
+              onChange={(e) =>
+                setCompanyData({ ...companyData, phone: e.target.value })
+              }
+            />
+          </div>
+          <div className="flex flex-col gap-y-2">
+            <Text className="font-medium text-neutral-950">Address</Text>
+            <Input
+              label="Address"
+              name="address"
+              value={companyData.address || ""}
+              onChange={(e) =>
+                setCompanyData({ ...companyData, address: e.target.value })
+              }
+            />
+          </div>
+          <div className="flex flex-col gap-y-2">
+            <Text className="font-medium text-neutral-950">City</Text>
+            <Input
+              label="City"
+              name="city"
+              value={companyData.city || ""}
+              onChange={(e) =>
+                setCompanyData({ ...companyData, city: e.target.value })
+              }
+            />
+          </div>
+          <div className="flex flex-col gap-y-2">
+            <Text className="font-medium text-neutral-950">State</Text>
+            <Input
+              label="State"
+              name="state"
+              value={companyData.state || ""}
+              onChange={(e) =>
+                setCompanyData({ ...companyData, state: e.target.value })
+              }
+            />
+          </div>
+          <div className="flex flex-col gap-y-2">
+            <Text className="font-medium text-neutral-950">Zip</Text>
+            <Input
+              label="Zip"
+              name="zip"
+              value={companyData.zip || ""}
+              onChange={(e) =>
+                setCompanyData({ ...companyData, zip: e.target.value })
+              }
+            />
+          </div>
+          <div className="flex flex-col gap-y-2">
+            <Text className="font-medium text-neutral-950">Country</Text>
+            <Select
+              name="country"
+              value={companyData.country || ""}
+              onChange={(e) =>
+                setCompanyData({ ...companyData, country: e.target.value })
+              }
+            >
+              {countriesInRegions.map(
+                (country) =>
+                  country && (
+                    <option key={country.id} value={country.id}>
+                      {country.name}
                     </option>
                   )
-                )}
-              </Select>
-            </div>
-          </form>
-        ) : (
-          <div className="grid grid-cols-2 gap-4 p-4 border-b border-neutral-200">
-            <div className="flex flex-col gap-y-2">
-              <Text className="font-medium text-neutral-950">Company Name</Text>
-              <Text className=" text-neutral-500">{company.name}</Text>
-            </div>
-            <div className="flex flex-col gap-y-2">
-              <Text className="font-medium text-neutral-950">Email</Text>
-              <Text className=" text-neutral-500">{company.email}</Text>
-            </div>
-            <div className="flex flex-col gap-y-2">
-              <Text className="font-medium text-neutral-950">Phone</Text>
-              <Text className=" text-neutral-500">{company.phone}</Text>
-            </div>
-            <div className="flex flex-col gap-y-2">
-              <Text className="font-medium text-neutral-950">Address</Text>
-              <Text className=" text-neutral-500">
-                {company.address}, {company.city}, {company.state},{" "}
-                {company.zip}, {company.country?.toUpperCase()}
-              </Text>
-            </div>
-            <div className="flex flex-col gap-y-2">
-              <Text className="font-medium text-neutral-950">Currency</Text>
-              <Text className=" text-neutral-500">
-                {company.currency_code?.toUpperCase()} (
-                {currencySymbolMap[company.currency_code!]})
-              </Text>
-            </div>
-            <div className="flex flex-col gap-y-2">
-              <Text className="font-medium text-neutral-950">
-                Spending Limit Reset Frequency
-              </Text>
-              <Text className=" text-neutral-500">
-                {company.spending_limit_reset_frequency
-                  ?.charAt(0)
-                  .toUpperCase() +
-                  company.spending_limit_reset_frequency?.slice(1)}
-              </Text>
-            </div>
+              )}
+            </Select>
           </div>
-        )}
+          <div className="flex flex-col gap-y-2">
+            <Text className="font-medium text-neutral-950">Currency</Text>
+            <Select
+              name="currency_code"
+              value={companyData.currency_code || ""}
+              onChange={(e) =>
+                setCompanyData({
+                  ...companyData,
+                  currency_code: e.target.value as string,
+                })
+              }
+            >
+              {currenciesInRegions.map((currency) => (
+                <option key={currency} value={currency}>
+                  {currency.toUpperCase()} ({currencySymbolMap[currency]})
+                </option>
+              ))}
+            </Select>
+          </div>
+          <div className="flex flex-col gap-y-2">
+            <Text className="font-medium text-neutral-950">
+              Spending Limit Reset Frequency
+            </Text>
+            <Select
+              name="spending_limit_reset_frequency"
+              value={companyData.spending_limit_reset_frequency}
+              onChange={(e) =>
+                setCompanyData({
+                  ...companyData,
+                  spending_limit_reset_frequency: e.target
+                    .value as ModuleCompanySpendingLimitResetFrequency,
+                })
+              }
+            >
+              {Object.values(ModuleCompanySpendingLimitResetFrequency).map(
+                (value) => (
+                  <option key={value} value={value}>
+                    {value.charAt(0).toUpperCase() + value.slice(1)}
+                  </option>
+                )
+              )}
+            </Select>
+          </div>
+        </form>
+        <div
+          className={clx(
+            "grid grid-cols-2 gap-4 border-b border-neutral-200 transition-all duration-300 ease-in-out",
+            {
+              "opacity-0 max-h-0": isEditing,
+              "opacity-100 max-h-[280px] p-4": !isEditing,
+            }
+          )}
+        >
+          <div className="flex flex-col gap-y-2">
+            <Text className="font-medium text-neutral-950">Company Name</Text>
+            <Text className=" text-neutral-500">{company.name}</Text>
+          </div>
+          <div className="flex flex-col gap-y-2">
+            <Text className="font-medium text-neutral-950">Email</Text>
+            <Text className=" text-neutral-500">{company.email}</Text>
+          </div>
+          <div className="flex flex-col gap-y-2">
+            <Text className="font-medium text-neutral-950">Phone</Text>
+            <Text className=" text-neutral-500">{company.phone}</Text>
+          </div>
+          <div className="flex flex-col gap-y-2">
+            <Text className="font-medium text-neutral-950">Address</Text>
+            <Text className=" text-neutral-500">
+              {company.address}, {company.city}, {company.state}, {company.zip},{" "}
+              {company.country?.toUpperCase()}
+            </Text>
+          </div>
+          <div className="flex flex-col gap-y-2">
+            <Text className="font-medium text-neutral-950">Currency</Text>
+            <Text className=" text-neutral-500">
+              {company.currency_code?.toUpperCase()} (
+              {currencySymbolMap[company.currency_code!]})
+            </Text>
+          </div>
+          <div className="flex flex-col gap-y-2">
+            <Text className="font-medium text-neutral-950">
+              Spending Limit Reset Frequency
+            </Text>
+            <Text className=" text-neutral-500">
+              {company.spending_limit_reset_frequency?.charAt(0).toUpperCase() +
+                company.spending_limit_reset_frequency?.slice(1)}
+            </Text>
+          </div>
+        </div>
+
         <div className="flex items-center justify-end gap-2 bg-neutral-50 p-4">
           {isEditing ? (
             <>
--- apps/storefront/src/modules/account/components/employees-card/employee.tsx
+++ apps/storefront/src/modules/account/components/employees-card/employee.tsx
@@ -7,7 +7,7 @@ import {
   getSpendWindow,
 } from "@lib/util/check-spending-limit"
 import { HttpTypes } from "@medusajs/types"
-import { CurrencyInput, Prompt, Text, Toaster, toast } from "@medusajs/ui"
+import { CurrencyInput, Prompt, Text, Toaster, clx, toast } from "@medusajs/ui"
 import { formatAmount } from "@modules/common/components/amount-cell"
 import Button from "@modules/common/components/button"
 import NativeSelect from "@modules/common/components/native-select"
@@ -162,53 +162,55 @@ const Employee = ({
           )}
         </div>
       </div>
-      {isEditing && (
-        <form
-          className="bg-neutral-50 p-4 grid grid-cols-2 gap-4 border-b border-neutral-200"
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              e.preventDefault()
-              handleSubmit()
-            }
-          }}
-        >
-          <div className="flex flex-col gap-y-2">
-            <Text className=" text-neutral-950 font-medium">
-              Spending Limit
-            </Text>
-            <CurrencyInput
-              symbol={currencySymbolMap[company.currency_code!]}
-              code={company.currency_code!}
-              className="bg-white rounded-full"
-              name="spending_limit"
-              value={employeeData.spending_limit}
-              onChange={(e) => {
-                setEmployeeData({
-                  ...employeeData,
-                  spending_limit: e.target.value.replace(/[^0-9.]/g, ""),
-                })
-              }}
-            />
-          </div>
-          <div className="flex flex-col gap-y-2">
-            <Text className=" text-neutral-950 font-medium">Permissions</Text>
-            <NativeSelect
-              className="bg-white"
-              name="permissions"
-              value={employeeData.is_admin ? "true" : "false"}
-              onChange={(e) => {
-                setEmployeeData({
-                  ...employeeData,
-                  is_admin: e.target.value === "true",
-                })
-              }}
-            >
-              <option value="true">Admin</option>
-              <option value="false">Employee</option>
-            </NativeSelect>
-          </div>
-        </form>
-      )}
+      <form
+        className={clx(
+          "bg-neutral-50 grid grid-cols-2 gap-4 border-b border-neutral-200 transition-all duration-300 ease-in-out",
+          {
+            "max-h-[98px] opacity-100 p-4": isEditing,
+            "max-h-0 h-0 opacity-0 border-b-0": !isEditing,
+          }
+        )}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault()
+            handleSubmit()
+          }
+        }}
+      >
+        <div className="flex flex-col gap-y-2">
+          <Text className=" text-neutral-950 font-medium">Spending Limit</Text>
+          <CurrencyInput
+            symbol={currencySymbolMap[company.currency_code!]}
+            code={company.currency_code!}
+            className="bg-white rounded-full"
+            name="spending_limit"
+            value={employeeData.spending_limit}
+            onChange={(e) => {
+              setEmployeeData({
+                ...employeeData,
+                spending_limit: e.target.value.replace(/[^0-9.]/g, ""),
+              })
+            }}
+          />
+        </div>
+        <div className="flex flex-col gap-y-2">
+          <Text className=" text-neutral-950 font-medium">Permissions</Text>
+          <NativeSelect
+            className="bg-white"
+            name="permissions"
+            value={employeeData.is_admin ? "true" : "false"}
+            onChange={(e) => {
+              setEmployeeData({
+                ...employeeData,
+                is_admin: e.target.value === "true",
+              })
+            }}
+          >
+            <option value="true">Admin</option>
+            <option value="false">Employee</option>
+          </NativeSelect>
+        </div>
+      </form>
       <Toaster />
     </div>
   )
