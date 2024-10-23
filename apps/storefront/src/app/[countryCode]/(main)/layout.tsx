3b554e7b4917fce8db19a2a2b51eaeeb3062861b
Victor Gerbrands <victorgerbrands@gmail.com>
2024-10-23 16:08:41 +0200

    feat: producthunt banner

--- apps/storefront/src/app/[countryCode]/(main)/layout.tsx
+++ apps/storefront/src/app/[countryCode]/(main)/layout.tsx
@@ -1,10 +1,8 @@
-import { Metadata } from "next"
-
 import { getBaseURL } from "@lib/util/env"
 import { ArrowUpRightMini, ExclamationCircleSolid } from "@medusajs/icons"
-import LocalizedClientLink from "@modules/common/components/localized-client-link"
 import Footer from "@modules/layout/templates/footer"
 import { NavigationHeader } from "@modules/layout/templates/nav"
+import { Metadata } from "next"
 
 export const metadata: Metadata = {
   metadataBase: new URL(getBaseURL()),
@@ -18,17 +16,17 @@ export default async function PageLayout(props: { children: React.ReactNode }) {
         <div className="flex flex-col small:flex-row small:gap-2 gap-1 items-center">
           <span className="flex items-center gap-1">
             <ExclamationCircleSolid className="inline" color="#A1A1AA" />
-            Favorites at a great price! For a limited time only
+            Medusa 2.0 is here! Support us on Product Hunt:
           </span>
-          <span className="hidden small:inline">·</span>
 
-          <LocalizedClientLink
+          <a
             className="group hover:text-ui-fg-interactive-hover text-ui-fg-interactive self-end small:self-auto"
-            href="/"
+            href="https://dub.sh/medusa-hunt"
+            target="_blank"
           >
-            Go to Products
+            Upvote Launch
             <ArrowUpRightMini className="group-hover:text-ui-fg-interactive-hover inline text-ui-fg-interactive" />
-          </LocalizedClientLink>
+          </a>
         </div>
       </div>
       {props.children}
