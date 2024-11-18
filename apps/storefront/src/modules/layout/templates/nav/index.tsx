bf8f244336225c1cc5f9dfd753b10e160ae3ffb4
Riqwan Thamir <rmthamir@gmail.com>
2024-11-18 14:00:17 +0100

    chore: update text color

--- apps/storefront/src/modules/layout/templates/nav/index.tsx
+++ apps/storefront/src/modules/layout/templates/nav/index.tsx
@@ -44,7 +44,7 @@ export async function NavigationHeader() {
               <input
                 type="text"
                 placeholder="Search for products"
-                className="bg-gray-100 text-white px-4 py-2 rounded-full pr-10 shadow-borders-base hidden small:inline-block"
+                className="bg-gray-100 text-zinc-900 px-4 py-2 rounded-full pr-10 shadow-borders-base hidden small:inline-block"
               />
             </div>
 
