c127fa30c561867c0a5632c6152d9cfc9086b4ca
Victor Gerbrands <victorgerbrands@gmail.com>
2024-12-18 21:37:25 +0100

    chore: fix module import

--- backend/src/workflows/employee/workflows/create-employees.ts
+++ backend/src/workflows/employee/workflows/create-employees.ts
@@ -3,7 +3,7 @@ import { when } from "@medusajs/framework/workflows-sdk";
 import { createRemoteLinkStep } from "@medusajs/medusa/core-flows";
 import { createWorkflow, WorkflowResponse } from "@medusajs/workflows-sdk";
 import { ModuleCreateEmployee, ModuleEmployee } from "@starter/types";
-import { COMPANY_MODULE } from "src/modules/company";
+import { COMPANY_MODULE } from "../../../modules/company";
 import { createEmployeesStep, setAdminRoleStep } from "../steps";
 import { addEmployeeToCustomerGroupStep } from "../steps/add-employee-to-customer-group";
 
