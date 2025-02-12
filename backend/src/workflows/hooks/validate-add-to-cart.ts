6d5b62bb27b8bb1ab585ec487578e0cc3245d4e2
Victor Gerbrands <victorgerbrands@gmail.com>
2025-02-12 13:13:52 +0100

    fix: util import

--- backend/src/workflows/hooks/validate-add-to-cart.ts
+++ backend/src/workflows/hooks/validate-add-to-cart.ts
@@ -1,7 +1,7 @@
 import { ContainerRegistrationKeys } from "@medusajs/framework/utils";
 import { StepResponse } from "@medusajs/framework/workflows-sdk";
 import { addToCartWorkflow } from "@medusajs/medusa/core-flows";
-import { getCartApprovalStatus } from "src/utils/get-cart-approval-status";
+import { getCartApprovalStatus } from "../../utils/get-cart-approval-status";
 
 addToCartWorkflow.hooks.validate(async ({ cart }, { container }) => {
   const query = container.resolve(ContainerRegistrationKeys.QUERY);
