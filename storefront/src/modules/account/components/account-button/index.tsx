0a531f5d988f499a94f26b85edd3a014e66e36ab
Victor Gerbrands <victorgerbrands@gmail.com>
2025-04-28 13:11:23 +0200

    fix: same

--- storefront/src/modules/account/components/account-button/index.tsx
+++ storefront/src/modules/account/components/account-button/index.tsx
@@ -1,10 +1,12 @@
-import { retrieveCustomer } from "@/lib/data/customer"
 import LocalizedClientLink from "@/modules/common/components/localized-client-link"
 import User from "@/modules/common/icons/user"
+import { B2BCustomer } from "@/types/global"
 
-export default async function AccountButton() {
-  const customer = await retrieveCustomer().catch(() => null)
-
+export default async function AccountButton({
+  customer,
+}: {
+  customer: B2BCustomer | null
+}) {
   return (
     <LocalizedClientLink className="hover:text-ui-fg-base" href="/account">
       <button className="flex gap-1.5 items-center rounded-2xl bg-none shadow-none border-none hover:bg-neutral-100 px-2 py-1">
