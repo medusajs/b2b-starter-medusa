b6db1152da06a9c78bcab8c3dbe2f6e098ce28e4
Azim Jiwani <azimjiwani4@gmail.com>
2025-05-23 01:37:10 -0400

    update banner for log in and approval status

--- storefront/src/app/[countryCode]/(main)/layout.tsx
+++ storefront/src/app/[countryCode]/(main)/layout.tsx
@@ -7,7 +7,7 @@ import Footer from "@/modules/layout/templates/footer"
 import { NavigationHeader } from "@/modules/layout/templates/nav"
 import FreeShippingPriceNudge from "@/modules/shipping/components/free-shipping-price-nudge"
 import { StoreFreeShippingPrice } from "@/types/shipping-option/http"
-import { ArrowUpRightMini, ExclamationCircleSolid } from "@medusajs/icons"
+import { ExclamationCircleSolid } from "@medusajs/icons"
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
@@ -26,23 +26,18 @@ export default async function PageLayout(props: { children: React.ReactNode }) {
   return (
     <>
       <NavigationHeader />
-      <div className="flex items-center text-neutral-50 justify-center small:p-4 p-2 text-center bg-neutral-900 small:gap-2 gap-1 text-sm">
-        <div className="flex flex-col small:flex-row small:gap-2 gap-1 items-center">
-          <span className="flex items-center gap-1">
-            <ExclamationCircleSolid className="inline" color="#A1A1AA" />
-            Build your own B2B store with this starter:
-          </span>
-
-          <a
-            className="group hover:text-ui-fg-interactive-hover text-ui-fg-interactive self-end small:self-auto"
-            href="https://git.new/b2b-starter-repo"
-            target="_blank"
-          >
-            GitHub Repo
-            <ArrowUpRightMini className="group-hover:text-ui-fg-interactive-hover inline text-ui-fg-interactive" />
-          </a>
+      {customer && !customer.metadata?.approved && (
+        <div className="flex items-center text-white justify-center small:p-4 p-2 text-center bg-red-600 small:gap-2 gap-1 text-sm">
+          <div className="flex flex-col small:flex-row small:gap-2 gap-1 items-center">
+            <span className="flex items-center gap-1">
+              <ExclamationCircleSolid className="inline" />
+              Your account status is set to unapproved. Please get in touch with us to
+              change this and get the full experience. See product prices, add them to
+              the cart, and check out.
+            </span>
+          </div>
         </div>
-      </div>
+      )}
 
       {customer && cart && (
         <CartMismatchBanner customer={customer} cart={cart} />
