31fc3cd0fa75b7f23f74ff8b23d87e66755ebaa7
Roger Noble <roger@nobledynamic.com>
2025-06-05 18:21:49 +0100

    Fixed lint errors

--- storefront/.eslintrc.js
+++ storefront/.eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
-  extends: ["next/core-web-vitals"]
+  extends: ["next/core-web-vitals"],
+  rules: {
+    "@next/next/no-html-link-for-pages": "off",
+    "@next/next/no-page-custom-font": "off",
+    "@next/next/no-typos": "off",
+    "@next/next/no-duplicate-head": "off",
+    "@next/next/no-before-interactive-script-outside-document": "off",
+    "@next/next/no-styled-jsx-in-document": "off",
+  }
 };
