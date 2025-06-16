0b4706dac0e54a1d537db29b7ad48a055be6c128
Victor Gerbrands <victorgerbrands@gmail.com>
2025-06-16 16:23:23 +0200

    fix: middleware method for allowing employee on /me

--- backend/src/api/middlewares.ts
+++ backend/src/api/middlewares.ts
@@ -13,7 +13,6 @@ export default defineMiddlewares({
     ...storeMiddlewares,
     {
       matcher: "/store/customers/me",
-      method: "GET",
       middlewares: [
         (req: MedusaRequest, res: MedusaResponse, next: MedusaNextFunction) => {
           req.allowed = ["employee"];
