92a8d79f80dcd5d7d79ce42eb795eb8c744c134f
own-boldsbrain <fernando@yellosolarhub.com>
2025-10-08 00:08:26 -0300

    refactor: simplify setup.ts by removing manual JSDOM configuration and improving TextEncoder/TextDecoder import comment

--- storefront/src/__tests__/setup.ts
+++ storefront/src/__tests__/setup.ts
@@ -8,34 +8,10 @@ configure({
     reactStrictMode: true,
 })
 
-// Importar TextEncoder/TextDecoder antes do JSDOM
+// Importar TextEncoder/TextDecoder para compatibilidade
 import { TextEncoder, TextDecoder } from 'util'
 global.TextEncoder = TextEncoder as any
 global.TextDecoder = TextDecoder as any
-
-// Configuração manual do jsdom para React 19
-import { JSDOM } from 'jsdom'
-
-const dom = new JSDOM('<!DOCTYPE html><html><body></body></html>', {
-    url: 'http://localhost',
-    pretendToBeVisual: true,
-    resources: 'usable',
-})
-
-global.window = dom.window as any
-global.document = dom.window.document
-global.navigator = dom.window.navigator
-global.HTMLElement = dom.window.HTMLElement
-global.HTMLInputElement = dom.window.HTMLInputElement
-global.HTMLButtonElement = dom.window.HTMLButtonElement
-global.HTMLDivElement = dom.window.HTMLDivElement
-
-// Copiar propriedades do window para global
-Object.keys(dom.window).forEach(key => {
-    if (!(key in global)) {
-        (global as any)[key] = (dom.window as any)[key]
-    }
-})
 
 // Mock do Next.js router
 jest.mock('next/navigation', () => ({
