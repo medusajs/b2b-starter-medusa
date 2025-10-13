0d28502054bb8c1d29815ecafa86fbdbda429b76
own-boldsbrain <fernando@yellosolarhub.com>
2025-10-13 10:38:26 -0300

    chore(backend/.vscode): add Python environment settings (defaultEnvManager, pythonProjects)

---
+++ storefront/src/components/error-boundary/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Error boundary resilient exports
+ */
+
+export {
+  ErrorBoundaryResilient,
+  withErrorBoundary,
+  useErrorBoundary,
+  type Props as ErrorBoundaryResilientProps,
+} from "./error-boundary-resilient"
