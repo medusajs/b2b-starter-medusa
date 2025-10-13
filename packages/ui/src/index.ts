c9690d9ac73874135402a55592fe2aeac579f666
own-boldsbrain <fernando@yellosolarhub.com>
2025-10-13 04:53:00 -0300

    feat: add YshGradientIcon and YshLogoMark components with gradient styles

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,9 @@
+export { YelloSolarButton as YshButton } from "./components/yello-solar-button"
+export { YelloSolarAlert as YshAlert, SuccessAlert as YshSuccessAlert, ErrorAlert as YshErrorAlert, InfoAlert as YshInfoAlert, WarningAlert as YshWarningAlert } from "./components/yello-solar-alert"
+export { YshGradientIcon } from "./components/YshGradientIcon"
+export { YshLogoMark } from "./components/YshLogoMark"
+
+// CSS entrypoints (consumer should import these in their app root)
+export const yshThemeCss = "@ysh/ui/styles/theme.css"
+export const yshGradientsCss = "@ysh/ui/styles/gradients.css"
+
